d68e372e798f33f90f7fdc000287b813c977763b
H-M-Sufyan <hmsufyan61@gmail.com>
2025-11-08 00:43:18 -0800

    category section add

--- lib/screens/user-panel/main-screen.dart
+++ lib/screens/user-panel/main-screen.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:product_hub/utils/colors.dart';
 import 'package:product_hub/widgets/banner.dart';
+import 'package:product_hub/widgets/categories-section.dart';
 import 'package:product_hub/widgets/search-bar.dart';
+
+import '../../models/category-model.dart';
 
 class UserMainScreen extends StatelessWidget {
   const UserMainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+
+    // categories List
+    final categories = [
+      CategoryModel(name: "Foods", icon: Icons.fastfood, color: Colors.orange),
+      CategoryModel(
+        name: "Gifts",
+        icon: Icons.card_giftcard,
+        color: Colors.purple,
+      ),
+      CategoryModel(name: "Furniture", icon: Icons.chair, color: Colors.green),
+      CategoryModel(
+        name: "Fashion",
+        icon: Icons.shopping_bag,
+        color: Colors.pink,
+      ),
+      CategoryModel(
+        name: "Electronics",
+        icon: Icons.electrical_services_rounded,
+        color: Colors.orange,
+      ),
+    ];
+    // categories List
+
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -30,8 +58,59 @@ class UserMainScreen extends StatelessWidget {
           child: Column(
             children: [
               CustomSearchBar(),
-              BannerSection()
-              
+              BannerSection(),
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Categories",
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      // Navigator.push(
+                      //   context,
+                      //   MaterialPageRoute(builder: (context) => ),
+                      // );
+                    },
+                    child: Text(
+                      'See All',
+                      style: TextStyle(
+                        color: AppColors.buttonColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              CategoriesSection(categories: categories),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Featured Product",
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      // Navigator.push(
+                      //   context,
+                      //   MaterialPageRoute(builder: (context) => ),
+                      // );
+                    },
+                    child: Text(
+                      'See All',
+                      style: TextStyle(
+                        color: AppColors.buttonColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
